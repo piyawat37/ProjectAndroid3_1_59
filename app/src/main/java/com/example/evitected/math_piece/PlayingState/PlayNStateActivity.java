ac241212f6a0e81898873c6f0d08cce92f41cc37
piyawat37 <piyawat.pem.it57@cpc.ac.th>
2016-12-05 19:02:00 +0700

    State Classify

--- app/src/main/java/com/example/evitected/math_piece/PlayingState/PlayNStateActivity.java
+++ app/src/main/java/com/example/evitected/math_piece/PlayingState/PlayNStateActivity.java
@@ -1,12 +1,10 @@
 package com.example.evitected.math_piece.PlayingState;
 
-import android.content.ClipData;
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.DragEvent;
-import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
@@ -16,15 +14,21 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.evitected.math_piece.CustomAdapter;
+import com.example.evitected.math_piece.ClassModel.CustomAdapter;
 import com.example.evitected.math_piece.GridModel.GridLevel;
 import com.example.evitected.math_piece.R;
 
 public class PlayNStateActivity extends AppCompatActivity {
 
-    private Button btnBack;
-    private GridView gvLevel_1;
     GridLevel gvObj = new GridLevel();
+    private TextView test;
+    int state;
+    int[] answer = new int[25];
+    private String oldText;
+    private GridView gvAnswer;
+    private ImageView imgQuestion;
+    private TextView tvAnswer;
+    private Button btnSubmit, btnEscape;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,67 +36,178 @@ public class PlayNStateActivity extends AppCompatActivity {
         setContentView(R.layout.activity_play_nstate);
         getSupportActionBar().hide();
         bindWidget();
-        setFontAwesome();
-        customAdap();
-    }
-    private void customAdap() {
-        gvLevel_1.setAdapter(new CustomAdapter(getApplicationContext(), gvObj.getLevel_1()));
-        gvLevel_1.setOnDragListener(new DragListener());
+        setAnswer();
+        //setFontAwesome();
+        create_GridAnswer();
+        getState();
+        setEventButton();
     }
 
-    class DragListener implements View.OnDragListener {
-        @Override
-        public boolean onDrag(View v, DragEvent event) {
-            final View view = (View) event.getLocalState();
-            switch (event.getAction()) {
-                case DragEvent.ACTION_DRAG_LOCATION:
-                    // do nothing if hovering above own position
-                    if (view == v) return true;
-                    // get the new list index
-                    final int index = calculateNewIndex(event.getX(), event.getY());
-                    //remove the view from the old position
-                    gvLevel_1.removeView(view);
-                    // and push to the new
-                    gvLevel_1.addView(view, index);
-                    break;
-                case DragEvent.ACTION_DROP:
-                    view.setVisibility(View.VISIBLE);
-                    break;
-                case DragEvent.ACTION_DRAG_ENDED:
-                    if (!event.getResult()) {
-                        view.setVisibility(View.VISIBLE);
-                    }
-                    break;
+
+    private void create_GridAnswer() {
+        gvAnswer.setAdapter(new CustomAdapter(getApplicationContext(), gvObj.getAnswer()));
+        gvAnswer.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_MOVE){
+                    return true;
+                }
+                return false;
             }
-            return true;
+        });
+        gvAnswer_Click();
+    }
+
+    private void gvAnswer_Click() {
+        gvAnswer.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(position == 0){
+                    oldText = tvAnswer.getText().toString().trim();
+                    tvAnswer.setText(oldText + "0");
+                }else if(position == 1){
+                    oldText = tvAnswer.getText().toString().trim();
+                    tvAnswer.setText(oldText + "1");
+                }else if(position == 2){
+                    oldText = tvAnswer.getText().toString().trim();
+                    tvAnswer.setText(oldText + "2");
+                }else if(position == 3){
+                    oldText = tvAnswer.getText().toString().trim();
+                    tvAnswer.setText(oldText + "3");
+                }else if(position == 4){
+                    oldText = tvAnswer.getText().toString().trim();
+                    tvAnswer.setText(oldText + "4");
+                }else if(position == 5){
+                    oldText = tvAnswer.getText().toString().trim();
+                    tvAnswer.setText(oldText + "5");
+                }else if(position == 6){
+                    oldText = tvAnswer.getText().toString().trim();
+                    tvAnswer.setText(oldText + "6");
+                }else if(position == 7){
+                    oldText = tvAnswer.getText().toString().trim();
+                    tvAnswer.setText(oldText + "7");
+                }else if(position == 8){
+                    oldText = tvAnswer.getText().toString().trim();
+                    tvAnswer.setText(oldText + "8");
+                }else if(position == 9){
+                    oldText = tvAnswer.getText().toString().trim();
+                    tvAnswer.setText(oldText + "9");
+                }
+            }
+        });
+    }
+
+
+    private void setEventButton() {
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!tvAnswer.getText().toString().trim().isEmpty()){
+                    int ans = Integer.parseInt(tvAnswer.getText().toString().trim());
+                    if(state == 1){
+                        if((ans == answer[0])){
+                            Toast.makeText(PlayNStateActivity.this, "true", Toast.LENGTH_SHORT).show();
+                        }else {
+                            Toast.makeText(PlayNStateActivity.this, "false", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }else{
+                    Toast.makeText(PlayNStateActivity.this, "false", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        btnEscape.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    if(!tvAnswer.getText().toString().isEmpty()){
+                        oldText = tvAnswer.getText().toString().trim();
+                        tvAnswer.setText(oldText.substring(0,oldText.length()-1));
+                    }else{
+
+                    }
+                }catch (NullPointerException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void getState() {
+        Intent i = getIntent();
+        state = i.getIntExtra("positionClick",0);
+        switch(state){
+            case 1: imgQuestion.setImageResource(R.drawable.quest1);
+                break;
+            case 2: imgQuestion.setImageResource(R.drawable.a0);
+                break;
+            case 3: test.setText(String.valueOf(state));
+                break;
+            case 4: test.setText(String.valueOf(state));
+                break;
+            case 5: test.setText(String.valueOf(state));
+                break;
+            case 6: test.setText(String.valueOf(state));
+                break;
+            case 7: test.setText(String.valueOf(state));
+                break;
+            case 8: test.setText(String.valueOf(state));
+                break;
+            case 9: test.setText(String.valueOf(state));
+                break;
+            case 10: test.setText(String.valueOf(state));
+                break;
+            case 11: test.setText(String.valueOf(state));
+                break;
+            case 12: test.setText(String.valueOf(state));
+                break;
+            case 13: test.setText(String.valueOf(state));
+                break;
+            case 14: test.setText(String.valueOf(state));
+                break;
+            case 15: test.setText(String.valueOf(state));
+                break;
+            case 16: test.setText(String.valueOf(state));
+                break;
+            case 17: test.setText(String.valueOf(state));
+                break;
+            case 18: test.setText(String.valueOf(state));
+                break;
+            case 19: test.setText(String.valueOf(state));
+                break;
+            case 20: test.setText(String.valueOf(state));
+                break;
+            case 21: test.setText(String.valueOf(state));
+                break;
+            case 22: test.setText(String.valueOf(state));
+                break;
+            case 23: test.setText(String.valueOf(state));
+                break;
+            case 24: test.setText(String.valueOf(state));
+                break;
+            case 25: test.setText(String.valueOf(state));
+                break;
         }
     }
-    private int calculateNewIndex(float x, float y) {
-        // calculate which column to move to
-        final float cellWidth = gvLevel_1.getWidth() / gvLevel_1.getNumColumns();
-        final int column = (int)(x / cellWidth);
 
-        // calculate which row to move to
-        final float cellHeight = gvLevel_1.getHeight() / gvLevel_1.getColumnWidth();
-        final int row = (int)Math.floor(y / cellHeight);
-
-        // the items in the GridLayout is organized as a wrapping list
-        // and not as an actual grid, so this is how to get the new index
-        int index = row * gvLevel_1.getNumColumns() + column;
-        if (index >= gvLevel_1.getChildCount()) {
-            index = gvLevel_1.getChildCount() - 1;
-        }
-
-        return index;
+    private void setAnswer() {
+        answer[0] = 3612;
     }
+
     private void setFontAwesome() {
         Typeface font = Typeface.createFromAsset(getAssets(), "fonts/fontawesome-webfont.ttf");
-        btnBack.setTypeface(font);
-        btnBack.setText("\uf053");
+        //btnBack.setTypeface(font);
+        //btnBack.setText("\uf053");
     }
 
     private void bindWidget() {
-        gvLevel_1 = (GridView) findViewById(R.id.gvLevel_1);
-        btnBack = (Button)findViewById( R.id.back);
+        test = (TextView) findViewById(R.id.test);
+        gvAnswer = (GridView) findViewById(R.id.gvAnswer);
+        imgQuestion = (ImageView) findViewById(R.id.imgQuestion);
+        tvAnswer = (TextView) findViewById(R.id.tvAnswer);
+
+        //Button
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+        btnEscape = (Button) findViewById(R.id.btnEscape);
     }
 }
