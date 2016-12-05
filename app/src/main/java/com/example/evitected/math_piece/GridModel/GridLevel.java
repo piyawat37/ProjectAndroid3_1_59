19994d283151348cd985de617868980055df1ac0
piyawat37 <piyawat.pem.it57@cpc.ac.th>
2016-12-05 18:56:18 +0700

    State Level Update 1.1

--- app/src/main/java/com/example/evitected/math_piece/GridModel/GridLevel.java
+++ app/src/main/java/com/example/evitected/math_piece/GridModel/GridLevel.java
@@ -6,6 +6,7 @@ import com.example.evitected.math_piece.R;
  * Created by Evitected on 24/11/2559.
  */
 public class GridLevel {
+    //State Level Update 1.0
     private int[] levelState = new int[]{
             R.drawable.c1, R.drawable.c2, R.drawable.c3, R.drawable.c4, R.drawable.c5,
             R.drawable.c6, R.drawable.c7, R.drawable.c8, R.drawable.c9, R.drawable.c10,
@@ -13,6 +14,16 @@ public class GridLevel {
             R.drawable.c16, R.drawable.c17, R.drawable.c18, R.drawable.c19, R.drawable.c20,
             R.drawable.c21, R.drawable.c22, R.drawable.c23, R.drawable.c24, R.drawable.c25,
     };
+
+    //State Level Update 1.1 5-12-59 18:29
+    private int[] LockStateDefault = new int[] {
+            R.drawable.c1, R.drawable.lock_state, R.drawable.lock_state, R.drawable.lock_state, R.drawable.lock_state,
+            R.drawable.lock_state, R.drawable.lock_state, R.drawable.lock_state, R.drawable.lock_state, R.drawable.lock_state,
+            R.drawable.lock_state, R.drawable.lock_state, R.drawable.lock_state, R.drawable.lock_state, R.drawable.lock_state,
+            R.drawable.lock_state, R.drawable.lock_state, R.drawable.lock_state, R.drawable.lock_state, R.drawable.lock_state,
+            R.drawable.lock_state, R.drawable.lock_state, R.drawable.lock_state, R.drawable.lock_state, R.drawable.lock_state,
+    };
+    //Answer Update 1.1
     private int[] Answer = new int[]{
             R.drawable.a0, R.drawable.a1, R.drawable.a2, R.drawable.a3, R.drawable.a4,
             R.drawable.a5, R.drawable.a6, R.drawable.a7, R.drawable.a8, R.drawable.a9,
@@ -24,5 +35,9 @@ public class GridLevel {
 
     public int[] getLevelState() {
         return levelState;
+    }
+
+    public int[] getLockStateDefault() {
+        return LockStateDefault;
     }
 }
