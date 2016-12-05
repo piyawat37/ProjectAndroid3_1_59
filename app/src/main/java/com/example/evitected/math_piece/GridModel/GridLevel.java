c7b3285905407bdcffea8350aee2af38319ec932
piyawat37 <piyawat.pem.it57@cpc.ac.th>
2016-12-05 12:46:18 +0700

    State Level Update 1.0

--- app/src/main/java/com/example/evitected/math_piece/GridModel/GridLevel.java
+++ app/src/main/java/com/example/evitected/math_piece/GridModel/GridLevel.java
@@ -6,26 +6,14 @@ import com.example.evitected.math_piece.R;
  * Created by Evitected on 24/11/2559.
  */
 public class GridLevel {
-    private String[] levelState = new String[]{
-            "1", "2", "3", "4", "5",
-            "6", "7", "8", "9", "10",
-            "11", "12", "13", "14", "15",
-            "16", "17", "18", "19", "20",
-            "21", "22", "23", "24", "25"
+    private int[] levelState = new int[]{
+            R.drawable.c1, R.drawable.c2, R.drawable.c3, R.drawable.c4, R.drawable.c5,
+            R.drawable.c6, R.drawable.c7, R.drawable.c8, R.drawable.c9, R.drawable.c10,
+            R.drawable.c11, R.drawable.c12, R.drawable.c13, R.drawable.c14, R.drawable.c15,
+            R.drawable.c16, R.drawable.c17, R.drawable.c18, R.drawable.c19, R.drawable.c20,
+            R.drawable.c21, R.drawable.c22, R.drawable.c23, R.drawable.c24, R.drawable.c25,
     };
-    private int[] level_1 = new int[]{
-            R.drawable.space_number,R.drawable.space_number,R.drawable.space_number,R.drawable.space_number,R.drawable.space_number,
-            R.drawable.space_number,R.drawable.a3,          R.drawable.space_number,R.drawable.space_number,R.drawable.space_number,
-            R.drawable.c1,          R.drawable.plus,        R.drawable.c2,          R.drawable.equals,      R.drawable.space_number,
-            R.drawable.space_number,R.drawable.space_number,R.drawable.space_number,R.drawable.space_number,R.drawable.space_number,
-            R.drawable.space_number,R.drawable.space_number,R.drawable.space_number,R.drawable.space_number,R.drawable.space_number,
-    };
-
-    public int[] getLevel_1() {
-        return level_1;
-    }
-
-    public String[] getLevelState() {
+    public int[] getLevelState() {
         return levelState;
     }
 }
